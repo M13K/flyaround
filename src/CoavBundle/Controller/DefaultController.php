f78830e6a88d776d3ae72aa53874225fa53d2a96
M13K <ag.koffi@hotmail.fr>
2017-04-03 22:16:05 +0200

    second release

---
+++ src/CoavBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoavBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CoavBundle:Default:index.html.twig');
+    }
+}
---
+++ src/CoavBundle/Entity/Flight.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Flight
+ *
+ * @ORM\Table(name="flight")
+ * @ORM\Entity(repositoryClass="CoavBundle\Repository\FlightRepository")
+ */
+class Flight
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="departure", type="string", length=4)
+     */
+    private $departure;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="arrival", type="string", length=4)
+     */
+    private $arrival;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pilot", type="string", length=255)
+     */
+    private $pilot;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="freeSeats", type="integer")
+     */
+    private $freeSeats;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="takeofTime", type="datetime")
+     */
+    private $takeofTime;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set departure
+     *
+     * @param string $departure
+     *
+     * @return Flight
+     */
+    public function setDeparture($departure)
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    /**
+     * Get departure
+     *
+     * @return string
+     */
+    public function getDeparture()
+    {
+        return $this->departure;
+    }
+
+    /**
+     * Set arrival
+     *
+     * @param string $arrival
+     *
+     * @return Flight
+     */
+    public function setArrival($arrival)
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    /**
+     * Get arrival
+     *
+     * @return string
+     */
+    public function getArrival()
+    {
+        return $this->arrival;
+    }
+
+    /**
+     * Set pilot
+     *
+     * @param string $pilot
+     *
+     * @return Flight
+     */
+    public function setPilot($pilot)
+    {
+        $this->pilot = $pilot;
+
+        return $this;
+    }
+
+    /**
+     * Get pilot
+     *
+     * @return string
+     */
+    public function getPilot()
+    {
+        return $this->pilot;
+    }
+
+    /**
+     * Set freeSeats
+     *
+     * @param integer $freeSeats
+     *
+     * @return Flight
+     */
+    public function setFreeSeats($freeSeats)
+    {
+        $this->freeSeats = $freeSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get freeSeats
+     *
+     * @return int
+     */
+    public function getFreeSeats()
+    {
+        return $this->freeSeats;
+    }
+
+    /**
+     * Set takeofTime
+     *
+     * @param \DateTime $takeofTime
+     *
+     * @return Flight
+     */
+    public function setTakeofTime($takeofTime)
+    {
+        $this->takeofTime = $takeofTime;
+
+        return $this;
+    }
+
+    /**
+     * Get takeofTime
+     *
+     * @return \DateTime
+     */
+    public function getTakeofTime()
+    {
+        return $this->takeofTime;
+    }
+}
+
